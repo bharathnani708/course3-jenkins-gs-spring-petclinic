51ef9f242f3367b41a6ba4cb915cbc48dd2e099d
bharathnani708 <bharathnani708@gmail.com>
2024-03-05 16:34:16 -0500

    revert-2

--- src/test/java/org/springframework/samples/petclinic/vet/VetTests.java
+++ src/test/java/org/springframework/samples/petclinic/vet/VetTests.java
@@ -35,8 +35,6 @@ class VetTests {
 		assertThat(other.getFirstName()).isEqualTo(vet.getFirstName());
 		assertThat(other.getLastName()).isEqualTo(vet.getLastName());
 		assertThat(other.getId()).isEqualTo(vet.getId());
-				assertThat(other.getId()).isEqualTo(vet.getId());
-
 	}
 
 }
